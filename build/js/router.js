d78be1f645f0703c2786573dbce9183416decb60
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2025-06-29 23:47:28 +1000

    Rebuild with router improvements

--- build/js/router.js
+++ build/js/router.js
@@ -1,7 +1,21 @@
 import { dispatchEvent } from './events.js';
 
 const findStaticRouteByPath = (path) => routes.staticRoutes.find(route => route.path === path);
-const routeToRegexExpression = (path, route) => path.match(new RegExp(`^${route.path.replace(/:\w+/g, '(.+)')}$`));
+
+const routeToRegexExpression = (path) => {
+
+    for (const route of Object.values(routes.dynamicRoutes)) {
+
+        const regexPattern = route.path.replace(/:(\w+)/g, (_, name) => `(?<${name}>[^/]+)`);
+        const regex = new RegExp(`^${regexPattern}$`);
+        const match = path.match(regex);
+
+        if (match?.groups)
+            return route.url.replace(/:(\w+)/g, (_, name) => match.groups[name] || '');
+    }
+
+    return undefined;
+};
 
 const routes = {
     staticRoutes: [
@@ -15,7 +29,7 @@ const routes = {
     ],
     dynamicRoutes: [
         { path: '/portfolio/:id', url: '/html/portfolio/:id.html' },
-        { path: '/blog/:id', url: '/html/blog/:id.html' },
+        { path: '/blog/:year/:id', url: '/html/blog/:year/:id.html' },
     ]
 };
 
@@ -36,6 +50,7 @@ async function handleLocation() {
     // Check if the requested path matches a static route
     const staticRoute = findStaticRouteByPath(path);
     if (staticRoute) {
+        console.log(staticRoute.url);
         await render(staticRoute.url);
         dispatchEvent(path);
 
@@ -43,31 +58,30 @@ async function handleLocation() {
     }
 
     // Check if the requested path matches a dynamic route
-    for (const value of Object.values(routes.dynamicRoutes)) {
-
-        const match = routeToRegexExpression(path, value);
-        if (!match) {
-            continue;
-        }
-        const uri = value.url.replace(/:\w+/g, match[1]);
-        const apiUrl = (typeof process !== 'undefined') ? uri : `/search${uri}`;
-
-        await fetch(apiUrl).then((res) => {
-            if (res.status === 200) {
-                render(uri);
-                dispatchEvent(path);
-
-                return;
-            }
-            
-            // This dynamic route does not exist on the server
-            render(findStaticRouteByPath(404).url);        
-        });
-
+    const uri = routeToRegexExpression(path);
+    if (!uri) {
+        render(findStaticRouteByPath(404).url);
+        return;
     }
 
+    const url = (typeof process !== 'undefined') ? uri : `/search${uri}`;
+    await fetch(url).then((res) => {
+
+        console.log(res);
+
+        if (res.status === 200) {
+            render(uri);
+            dispatchEvent(path);
+
+            return;
+        }
+
+        // This dynamic route does not exist on the server
+        render(findStaticRouteByPath(404).url);
+    });
+
 }
-    
+
 
 async function render(route) {
     const main = document.getElementsByTagName("main");
